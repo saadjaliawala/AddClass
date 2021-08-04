008df2cfd4c64f0d034108aede820bcd99656595
Saad Mehmood <saad@code-avenue.com>
2021-08-04 22:22:21 +0500

    Mock Ui done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+
+const data = [
+  {heading: "Mark C" , checkboxNames : ["Vocab cards" , "sandpaper numerals" ,"Vocab cards" , "sandpaper numerals" ,"Vocab cards" ,]}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      
+      <select style={{border : "none" , borderStyle: "none" , backgroundColor: "white"}}  value="Radish">
+  <option value="Orange">Orange</option>
+  <option value="Radish">Radish</option>
+  <option value="Cherry">Cherry</option>
+</select>
+
+      <div style={{marginLeft: "5%" , marginRight: "5%" , display: "flex" , flexDirection: "row" , flexWrap: "wrap" , justifyContent: "center",}} >
+    <div  style={{overflowY: 'scroll',}}  class="child" >{data.map((item , index) => {
+      return(
+        <div style={{display: "flex" , flexDirection: "column" }} >
+          <h4 style={{textDecoration: "underline" , alignSelf: "flex-start"}} >{item.heading}</h4>
+          {item.checkboxNames.map((name , index) => {
+
+            return(
+              <div style={{display: "flex" , flexDirection: "row" , alignItems: "center"  }} >
+              <input type="checkbox"  />
+              <li>{name}</li>
+              </div>
+            );
+          })}
+        
+        </div>
+      );
+    })}</div>
+    <div class="child" >2</div>
+    <div class="child" >3</div>
+    <div class="child" >4</div>
+    <div class="child" >5</div>
+    <div class="child" >6</div>
+    <div class="child" >7</div>
+    <div class="child" >8</div>
+</div>
+
     </div>
   );
 }
